4b34fe167c3407902115d11998a1d7f1080c436f
svetlai <svetla_22@yahoo.com>
2017-05-20 21:14:12 +0300

    Edit RecentlyUsedList.

--- FSharpKatas/01-RecentlyUsedList/Program.fs
+++ FSharpKatas/01-RecentlyUsedList/Program.fs
@@ -7,33 +7,42 @@ open RecentlyUsedList
 let main argv = 
     let list = new RecentlyUsedList()
     
-    printfn "--Init list. \nCount: %A" list.Count
+    printfn "--Init list with default capacity \nCount: %A" list.Count
     printfn "Capacity: %A" list.Capacity
 
+//    list.Remove |> ignore // throws Invalid operation
     list.Add "first string" |> ignore
-    printfn "--Add item. \nGet: %A" list.Get
+    printfn "--Add item \nList: %O" list
     list.Add "second string"  |> ignore
-    printfn "--Add another item. \nGet: %A" list.Get
-//  list.Add "second string" |> ignore // throws Invalid argument
+    printfn "--Add another item \nList: %A" list
+    list.Add "first string" |> ignore
+    printfn "--Add non-unique item \nList: %A" list
     
     printfn "Count: %A" list.Count
     printfn "Element at index 0: %A" list.[0]
 //  list.[3] |> ignore // throws Index out of range.
 
-    list.Remove "second string" |> ignore
-    printfn "--Remove item. \nGet: %A" list.Get
-    printfn "Count: %A" list.Count
-    list.RemoveAtIndex 0 |> ignore
-    printfn "--Remove at index 0. \nGet: %A" list.Get
+    list.Remove |> ignore
+    printfn "--Remove item. \nList: %A" list
     printfn "Count: %A" list.Count
 
-    let listWithCapacity = new RecentlyUsedList(2)
-    printfn "-- New list with capacity. \nCapacity: %A" listWithCapacity.Capacity
-    listWithCapacity.Add "first string" |> ignore
-    listWithCapacity.Add "second string" |> ignore
+    let listWithCustomCapacity = new RecentlyUsedList(Some 2)
+    printfn "-- New list with capacity \nCapacity: %A" listWithCustomCapacity.Capacity
+    listWithCustomCapacity.Add "first string" |> ignore
+    listWithCustomCapacity.Add "second string" |> ignore
     printfn "Count: %A" list.Count
-    printfn "Add items. \nGet: %A" listWithCapacity.Get
-    listWithCapacity.Add "third string" |> ignore
-    printfn "Add item over capacity. \nGet: %A" listWithCapacity.Get
+    printfn "Add items \nList: %A" listWithCustomCapacity
+    listWithCustomCapacity.Add "third string" |> ignore
+    printfn "Add item over capacity \nList: %A" listWithCustomCapacity
 
+    let listWithNoCapacity = new RecentlyUsedList(None)
+    printfn "-- New list with no capacity \nCapacity: %A" listWithNoCapacity.Capacity
+    listWithNoCapacity.Add "first string" |> ignore
+    listWithNoCapacity.Add "second string" |> ignore
+    listWithNoCapacity.Add "third string" |> ignore
+    listWithNoCapacity.Add "forth string" |> ignore
+    listWithNoCapacity.Add "fifth string" |> ignore
+    listWithNoCapacity.Add "sixth string" |> ignore
+    printfn "Add items \nCount: %A" listWithNoCapacity.Count
+    printfn "List: %A" listWithNoCapacity
     0 // return an integer exit code
--- FSharpKatas/01-RecentlyUsedList/RecentlyUsedList.fs
+++ FSharpKatas/01-RecentlyUsedList/RecentlyUsedList.fs
@@ -3,58 +3,68 @@
 open System
 open System.Collections.Generic
 
-type RecentlyUsedList(capacity : int) =
-    let mutable items = List.empty
-
-    new() = new RecentlyUsedList(5)
-
-    member x.Get =
-        items
-            
-    member x.Add newItem = 
-        let isItemValid =
-            match newItem with
-            | "" | null -> nullArg "Adding null or empty value is not allowed."
-            | _ ->
-                let itemExists = items |> Seq.exists ((=) newItem)
-                match itemExists with
-                | true -> invalidArg newItem "The item already exists."
-                | false -> true
-
-        let result =
-            match isItemValid with
-            | true -> 
-                let hasReachedCapacity = items.Length = capacity
-                let items = 
-                    match hasReachedCapacity with
-                    | true -> 
-                        x.RemoveAtIndex (items.Length - 1) |> ignore
-                        items
-                    | false -> items
-                newItem :: items
-            | false -> items
-        items <- result
-        newItem
-
-    member x.Item with get(i:int) =
+type RecentlyUsedList(capacity : Option<int>) =
+    let mutable items : string list = List.empty
+    let validateIndex i validAction =
         let isValidIndex = 0 <= i && i < items.Length
         match isValidIndex with
-        | true -> items.[i]
+        | true -> validAction
         | false -> raise (new IndexOutOfRangeException "Index was out of range.")
 
-    member x.Remove itemToDelete =
+    let removeAtIndex i =
+        let itemToDelete = validateIndex i items.[i]
         let result  = 
             items |> List.filter (fun x -> x <> itemToDelete)
         items <- result
         itemToDelete
 
-    member x.RemoveAtIndex i =
-        let itemToDelete = x.[i]
-        x.Remove itemToDelete
+    new() =
+        new RecentlyUsedList(Some 5)
 
+    member x.Add newItem = 
+        let isItemValid =
+            match newItem with
+            | "" | null -> nullArg "Adding null or empty value is not allowed."
+            | _ -> true
+                
+        let result =
+            match isItemValid with
+            | true -> 
+                let hasReachedCapacity = 
+                    match capacity with
+                    | Some c -> items.Length = c
+                    | None -> false
+
+                let items = 
+                    match hasReachedCapacity with
+                    | true -> 
+                        removeAtIndex (items.Length - 1) |> ignore
+                        items
+                    | false -> items
+                
+                let items = 
+                    let existingItem = items |> Seq.tryFind ((=) newItem)
+                    match existingItem with
+                    | Some item -> items |> List.filter (fun x -> x <> item)
+                    | None -> items
+
+                newItem :: items
+            | false -> items
+        items <- result
+
+    member x.Item with get(i:int) =
+        validateIndex i items.[i]
+
+    member x.Remove =
+         match items.Length > 0 with
+            | true -> removeAtIndex 0
+            | false -> invalidOp "The list is empty."
+     
     member x.Count =
         items.Length
 
     member x.Capacity =
         capacity
 
+    override x.ToString() =
+        items.ToString()
